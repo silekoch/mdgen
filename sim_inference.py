69681ecb09d0f8ad1d1ef87f9e47c4b81ac1ebc9
Simon Koch <simon.leo.koch@tum.de>
2025-07-26 00:59:43 +0200

    Add CA conditioned inference

--- sim_inference.py
+++ sim_inference.py
@@ -13,7 +13,7 @@ parser.add_argument('--out_dir', type=str, default=".")
 parser.add_argument('--split', type=str, default='splits/4AA_test.csv')
 parser.add_argument('--c_alpha_only', action='store_true')
 parser.add_argument('--guide_by_known', action='store_true')
-parser.add_argument('--inpaint_details', action='store_true')
+parser.add_argument('--ca_cond', action='store_true')
 args = parser.parse_args()
 
 import os, torch, mdtraj, tqdm, time
@@ -26,18 +26,56 @@ from mdgen.utils import atom14_to_pdb, atom1_to_pdb
 from mdgen import residue_constants as rc
 import pandas as pd
 
-
-
-
 os.makedirs(args.out_dir, exist_ok=True)
 
 
+def get_batch_inpaint(name, seqres, num_frames):
+    arr = np.lib.format.open_memmap(f'{args.data_dir}/{name}{args.suffix}.npy', 'r')
+
+    arr = np.copy(arr[:num_frames]).astype(np.float32)
+    ca_coordinates = atom14_to_ca(torch.from_numpy(arr))  # (T, L, 3)
+    key_frames = ca_to_frames(ca_coordinates[0:1])
+    
+    arr = np.copy(arr[0:1]).astype(np.float32)
+
+    frames = atom14_to_frames(torch.from_numpy(arr))
+    seqres = torch.tensor([restype_order[c] for c in seqres])
+    atom37 = torch.from_numpy(atom14_to_atom37(arr, seqres[None])).float()
+    L = len(seqres)
+    mask = torch.ones(L)
+
+    torsions, torsion_mask = atom37_to_torsions(atom37, seqres[None])
+    return {
+        'ca_coordinates': ca_coordinates,
+        'key_frame_rots': key_frames._rots._rot_mats,
+        'key_frame_trans': key_frames._trans,
+        'torsions': torsions,
+        'torsion_mask': torsion_mask[0],
+        'trans': frames._trans,
+        'rots': frames._rots._rot_mats,
+        'seqres': seqres,
+        'mask': mask, # (L,)
+    }
+
+def rollout_inpaint(model, batch):
+    expanded_batch = {
+        'ca_coordinates': batch['ca_coordinates'],
+        'key_frame_rots': batch['key_frame_rots'],
+        'key_frame_trans': batch['key_frame_trans'],
+        'torsions': batch['torsions'].expand(-1, args.num_frames, -1, -1, -1),
+        'torsion_mask': batch['torsion_mask'],
+        'trans': batch['trans'].expand(-1, args.num_frames, -1, -1),
+        'rots': batch['rots'].expand(-1, args.num_frames, -1, -1, -1),
+        'seqres': batch['seqres'],
+        'mask': batch['mask'],
+    }
+
+    return model.inference(expanded_batch)
 
 def get_batch(name, seqres, num_frames):
     arr = np.lib.format.open_memmap(f'{args.data_dir}/{name}{args.suffix}.npy', 'r')
 
-    if not args.tps: # else keep all frames
-        arr = np.copy(arr[0:1]).astype(np.float32)
+    arr = np.copy(arr[0:1]).astype(np.float32)
 
     frames = atom14_to_frames(torch.from_numpy(arr))
     seqres = torch.tensor([restype_order[c] for c in seqres])
@@ -104,7 +142,10 @@ def rollout(model, batch):
     
     
 def do(model, name, seqres):
-    item = get_batch(name, seqres, num_frames = model.args.num_frames)
+    if args.ca_cond:
+        item = get_batch_inpaint(name, seqres, num_frames = model.args.num_frames)
+    else:
+        item = get_batch(name, seqres, num_frames = model.args.num_frames)
     batch = next(iter(torch.utils.data.DataLoader([item])))
 
     batch = tensor_tree_map(lambda x: x.cuda(), batch)
@@ -112,7 +153,10 @@ def do(model, name, seqres):
     all_trajs = []
     start = time.time()
     for _ in tqdm.trange(args.num_rollouts):
-        traj_out, _ = rollout(model, batch)
+        if args.ca_cond:
+            traj_out, _ = rollout_inpaint(model, batch)
+        else:
+            traj_out, _ = rollout(model, batch)
         all_trajs.append(traj_out[0])
 
     print(time.time() - start)
@@ -133,13 +177,8 @@ def do(model, name, seqres):
 
 @torch.no_grad()
 def main():
-    model = NewMDGenWrapper.load_from_checkpoint(args.sim_ckpt)
+    model = NewMDGenWrapper.load_from_checkpoint(args.sim_ckpt, guide_by_known=args.guide_by_known)
     model.eval().to('cuda')
-    
-    if args.inpaint_details:
-        raise NotImplementedError()
-        inpaint_model = NewMDGenWrapper.load_from_checkpoint(args.sim_ckpt, guide_by_known=args.guide_by_known)
-        inpaint_model.eval().to('cuda')
 
     df = pd.read_csv(args.split, index_col='name')
     names = np.array(df.index)
@@ -150,6 +189,9 @@ def main():
         if args.pdb_id and name not in args.pdb_id:
             continue
         if not os.path.exists(f'{args.data_dir}/{name}{args.suffix}.npy'):
+            continue
+        if len(df.seqres[name]) >= 256:
+            print(f'Skipping protein "{name}" with length {len(df.seqres[name])}')
             continue
         jobs.append(name)
     n_not_found = n_expected - len(jobs)
